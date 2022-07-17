414d32590344c8d543df1a4a78692e030573af38
Mateus Leite <mateus.lcsilva@outlook.com>
2022-07-17 13:59:12 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import './styles.css'
 import icon from '../../assets/img/notification-icon.svg'
+import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
+
+type Props = {
+  saleId: number;
+}
 
 
-const NotificationButton = () => {
+const NotificationButton = ( {saleId} : Props ) => {
+
+  const handleClick = (id:number) => {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+  }
+
+
     return (
-        <div className="dsmeta-red-btn-container">
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
           <img src={icon} alt="Notificar" />
         </div>
-      </div>
     )
 }
 
